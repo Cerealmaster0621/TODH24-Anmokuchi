88e19988db439c1a6ba8e4c9d38ffdba51355158
Cerealmaster0621 <cerealmaster@naver.com>
2024-08-12 20:30:29 +0900

    model config

--- test/main.py
+++ test/main.py
@@ -1,23 +1,24 @@
 import json
+import config
 import os
-from db import Connection
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-db = Connection('flask_mongo_crud')
 
-MONGODBPW = os.getenv("MONGODBPW")
-MONGODBUN = os.getenv("MONGODBUN")
-
-CONNECTION_STRING = f"mongodb+srv://{MONGODBUN}:{MONGODBPW}@anmokuchi.lyunw.mongodb.net/?retryWrites=true&w=majority&appName=Anmokuchi"
+CONNECTION_STRING = f"mongodb+srv://{config.MONGODBUN}:{config.MONGODBPW}@anmokuchi.lyunw.mongodb.net/?retryWrites=true&w=majority&appName=Anmokuchi"
 client = MongoClient(CONNECTION_STRING)
 db = client.get_database('flask_mongodb_atlas')
+collection = db.collection
 
-@app.route('/')
-def test():
-    db.db.collection.insert_one({"name":"John"})
-    return "success!"
+@app.route('/', methods=['GET'])
+def get_all_data():
+    data = list(collection.find({}))
+    for item in data:
+        item['_id'] = str(item['_id'])
+    return jsonify(data)
+
+print("PASSWORD: ",config.MONGODBPW)
 
 if __name__ == '__main__':
     app.run(port=3000)
